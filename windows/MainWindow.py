9bd5a44d1edd0da9649f9cd1b51e561813b8c6ad
julimer228 <julia-merta@wp.pl>
2023-10-22 11:39:06 +0200

    Added buttons

--- windows/MainWindow.py
+++ windows/MainWindow.py
@@ -6,6 +6,8 @@ from PyQt6 import QtCore
 from PyQt6 import QtGui
 import configparser
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QSize, Qt
+
 
 class MainWindow(QWidget):
     """A class representing the main window"""    
@@ -27,12 +29,31 @@ class MainWindow(QWidget):
         self.label_left = QLabel()
         self.label_left.setPixmap(self.left_image)
         
-        self.grid.addWidget(self.label_right,1,2)
-        self.grid.addWidget(self.label_left,1,1)
+        self.grid.addWidget(self.label_left,1,1, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.grid.addWidget(self.label_right,1,3, alignment=Qt.AlignmentFlag.AlignCenter)
         
+        prevBtn = QPushButton(
+            icon=QtGui.QIcon(config['DEFAULT']['icon-prev']),
+            parent=self
+        )
+        prevBtn.setFixedSize(100, 60)
+        prevBtn.setIconSize(QSize(80, 40))
+        
+        nextBtn = QPushButton(
+            icon=QtGui.QIcon(config['DEFAULT']['icon-next']),
+            parent=self,
+        )
+        nextBtn.setFixedSize(100, 60)
+        nextBtn.setIconSize(QSize(80, 40))
+        
+        buttons = QHBoxLayout()
+        buttons.addWidget(prevBtn)
+        buttons.addWidget(nextBtn)
+        
+        self.grid.addLayout(buttons,1,2,alignment=Qt.AlignmentFlag.AlignCenter)
         
         self.setLayout(self.grid)
-        self.setGeometry(50,50,320,200)
+        self.setGeometry(0,0,1920,1080)
         self.show()
     
     def update(self):
